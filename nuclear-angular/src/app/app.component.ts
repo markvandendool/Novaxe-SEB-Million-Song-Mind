d9d9e7257c8bc2dfe427f208d9f96a7a4517a798
markvandendool <markvandendool@me.com>
2025-08-17 09:56:03 -0600

    Cursor: Remove temporary audio test UI; audio proven with explicit WebAudioFont loading

--- nuclear-angular/src/app/app.component.ts
+++ nuclear-angular/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { GuitarService } from './services/guitar/guitar.service';
 
 @Component({
   selector: 'app-root',
@@ -9,30 +8,4 @@ import { GuitarService } from './services/guitar/guitar.service';
 })
 export class AppComponent {
   title = 'nuclear-angular';
-  audioUnlocked = false;
-
-  constructor(private guitarService: GuitarService) { }
-
-  unlockAudio() {
-    // Resume audio context if suspended (required by browsers)
-    if (this.guitarService['audioContext'] && this.guitarService['audioContext'].state === 'suspended') {
-      this.guitarService['audioContext'].resume();
-    }
-    this.audioUnlocked = true;
-    console.log('🔊 Audio unlocked!');
-  }
-
-  async testSound() {
-    if (!this.audioUnlocked) {
-      alert('Please click "Unlock Audio" first!');
-      return;
-    }
-
-    try {
-      console.log('🎵 Testing sound - playing C4 (MIDI 60)');
-      await this.guitarService.play(0, 60, 0.5); // C4 note
-    } catch (error) {
-      console.error('Test sound failed:', error);
-    }
-  }
 }
